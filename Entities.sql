1a90e2d3669de97d7c80c2c7f9e1cf0a951b31f8
Disputatio <p.felty.howe@gmail.com>
2024-02-17 18:03:10 -0500

    Refactored entities

---
+++ Entities.sql
@@ -0,0 +1,53 @@
+CREATE TABLE Person (
+    SSN varchar(255) PRIMARY KEY,
+    Name varchar(255),
+    Relationship varchar(255)
+);
+
+
+CREATE TABLE Employee (
+    SSN varchar(255),
+    MedicareNumber int PRIMARY KEY,
+    FOREIGN KEY (SSN) REFERENCES Person(SSN)
+);
+
+CREATE TABLE Facility (
+    Name varchar(255) PRIMARY KEY,
+    Type varchar(255),
+    Province varchar(255),
+    City varchar(255),
+    Address varchar(255),
+    Capacity int,
+    Website varchar(255),
+    Phone varchar(50),
+    PostalCode varchar(10)
+);
+
+CREATE TABLE Vaccine (
+    SSN int,
+    Type varchar(255),
+    VaccinationDate varchar(50),
+    Dose varchar(50),
+    Location varchar(255),
+    FOREIGN KEY (SSN) REFERENCES Person(SSN)
+);
+
+
+CREATE TABLE Infection (
+    SSN int,
+    InfectionDate varchar(255),
+    Amount int,
+    Type varchar(255),
+    FOREIGN KEY (SSN) REFERENCES Person(SSN)
+
+);
+
+CREATE TABLE Residence (
+    Address varchar(255) PRIMARY KEY,
+    City varchar(255),
+    Type varchar(255),
+    Province varchar(20),
+    PostalCode varchar(10),
+    PhoneNumber varchar(50),
+    Bedrooms int
+);
