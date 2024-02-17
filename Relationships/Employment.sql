00c67e5236d7480c3a8367f24a609a5992a33dd0
Disputatio <p.felty.howe@gmail.com>
2024-02-17 17:59:19 -0500

    Finished employement relationship

--- Relationships/Employment.sql
+++ Relationships/Employment.sql
@@ -1,14 +1,9 @@
 CREATE TABLE Employment (
     MedicareNumber int,
     HospitalName varchar(255),
-    FOREIGN KEY (MedicareNumber) REFERENCES Employee(MedicareNumber),
-    FOREIGN KEY (HospitalName) REFERENCES Facility(Name)
-);
-
-
-CREATE TABLE Managers (
-    MedicareNumber int,
-    HospitalName varchar(255),
+    Role varchar(255),
+    StartDate DATE,
+    EndDate DATE,
     FOREIGN KEY (MedicareNumber) REFERENCES Employee(MedicareNumber),
     FOREIGN KEY (HospitalName) REFERENCES Facility(Name)
 );
