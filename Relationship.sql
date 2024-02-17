0cf7d648355baa9e69f2608cc69b240284aaaf8a
Breno Carneiro de Faria <breno@Brenos-MacBook-Air.local>
2024-02-17 18:01:44 -0500

    relationship file created

--- Relationship.sql
+++ Relationship.sql
@@ -8,4 +8,11 @@ CREATE TABLE Employment (
     FOREIGN KEY (HospitalName) REFERENCES Facility(Name)
 );
 
-
+CREATE TABLE Residency (
+    SSN varchar(255),
+    Address varchar(255),
+    FOREIGN KEY (SSN) REFERENCES Employee(SSN) NOT NULL,
+    FOREIGN KEY (Address) REFERENCES Residence(Address) NOT NULL,
+    StartDate date,
+    EndDate date
+);
--- Relationships/Residency.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Residency (
-    SSN varchar(255),
-    Address varchar(255),
-    FOREIGN KEY (SSN) REFERENCES Employee(SSN) NOT NULL,
-    FOREIGN KEY (Address) REFERENCES Residence(Address) NOT NULL,
-    StartDate date,
-    EndDate date
-);
