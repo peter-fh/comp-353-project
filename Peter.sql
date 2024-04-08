caadd53f7cdc409f70b1e09518ab9385c8d4c842
Disputatio <p.felty.howe@gmail.com>
2024-04-08 18:46:09 -0400

    sdkuyfg

--- Peter.sql
+++ Peter.sql
@@ -1,14 +1,55 @@
-SELECT p.Name, e.Role, COUNT(r.SSN) AS 'Number of Secondary Residences'
-FROM Facility f
-JOIN Employment e ON f.Name = e.FacilityName
-JOIN Employee emp ON e.MedicareNumber = emp.MedicareNumber
-JOIN Person p ON emp.SSN = p.SSN
-JOIN Residency r ON p.SSN = r.SSN
-JOIN Schedule s ON emp.MedicareNumber = s.EmployeeMedicareNumber
-WHERE r.IsPrimary = 0
-AND s.ScheduleDate BETWEEN SUBDATE(CURDATE(), INTERVAL 7 DAY) AND CURDATE()
-GROUP BY p.Name, e.Role
+SELECT 
+    p.Name, 
+    e.Role, 
+    COUNT(CASE WHEN r.SSN = p.SSN THEN 1 ELSE NULL END) AS 'Number of Secondary Residences'
+FROM 
+    Facility f
+JOIN 
+    Employment e ON f.Name = e.FacilityName
+JOIN 
+    Employee emp ON e.MedicareNumber = emp.MedicareNumber
+JOIN 
+    Person p ON emp.SSN = p.SSN
+JOIN 
+    Residency r ON p.SSN = r.SSN
+JOIN 
+    Schedule s ON emp.MedicareNumber = s.EmployeeMedicareNumber
+WHERE 
+    r.IsPrimary = 0
+AND 
+    s.ScheduleDate BETWEEN SUBDATE(CURDATE(), INTERVAL 7 DAY) AND CURDATE()
+GROUP BY 
+    p.Name, e.Role
 HAVING COUNT(r.ssn) > 2;
 
+
+SELECT 
+    p.Name, 
+    e.Email, 
+    COUNT(CASE WHEN i.SSN = p.SSN THEN 1 ELSE NULL END) AS 'Number of Infections', 
+    COUNT(CASE WHEN i.SSN = p.SSN THEN 1 ELSE NULL END) AS 'Number of Vaccinations', 
+    SUM(TIMESTAMPDIFF(HOUR, s.StartTime, s.EndTime)) AS 'Total Hours Worked'
+FROM 
+    Employment emp 
+JOIN 
+    Employee e ON emp.MedicareNumber = e.MedicareNumber
+JOIN 
+    Person p ON e.SSN = p.SSN
+JOIN 
+    Schedule s ON emp.MedicareNumber = s.EmployeeMedicareNumber
+JOIN 
+    Infection i on p.SSN = i.SSN
+JOIN 
+    Vaccine v ON p.SSN = v.SSN
+WHERE 
+    emp.Role = 'Nurse'
+AND 
+    emp.EndDate IS NULL
+AND 	
+    i.InfectionDate BETWEEN SUBDATE(CURDATE(), INTERVAL 14 DAY) AND CURDATE()
+GROUP BY 
+    e.MedicareNumber
+HAVING 	
+    COUNT(DISTINCT emp.MedicareNumber) > 1;
 
 
